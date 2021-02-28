8c60c61a9881d83ee98e9b2f83f203299e86524a
Yuri Ershov <shtorm0606@gmail.com>
2021-02-28 08:34:27 +0300

    Refactor model class to use different priorities

--- CheckList/TodoList.swift
+++ CheckList/TodoList.swift
@@ -8,7 +8,18 @@
 import Foundation
 
 class TodoList {
-    var todos: [ChecklistItem] = []
+    enum Priority: Int, CaseIterable {
+        case high
+        case medium
+        case low
+        case none
+    }
+    
+    private var highPriorityTodos: [ChecklistItem] = []
+    private var mediumPriorityTodos: [ChecklistItem] = []
+    private var lowPriorityTodos: [ChecklistItem] = []
+    private var nonePriorityTodos: [ChecklistItem] = []
+    
     init() {
         
         let row0Item = ChecklistItem()
@@ -23,36 +34,75 @@ class TodoList {
         row3Item.text = "Walk a dog"
         row4Item.text = "Study design patterns"
         
-        todos.append(row0Item)
-        todos.append(row1Item)
-        todos.append(row2Item)
-        todos.append(row3Item)
-        todos.append(row4Item)
+        addToDo(row0Item, for: .medium)
+        addToDo(row1Item, for: .medium)
+        addToDo(row2Item, for: .medium)
+        addToDo(row3Item, for: .medium)
+        addToDo(row4Item, for: .medium)
+    }
+    
+    func toDoList(for priority: Priority) -> [ChecklistItem] {
+        switch priority {
+        case .high:
+            return highPriorityTodos
+        case .medium:
+            return mediumPriorityTodos
+        case .low:
+            return lowPriorityTodos
+        case .none:
+            return nonePriorityTodos
+        }
+    }
+    
+    func addToDo(_ item: ChecklistItem, for priority: Priority) {
+        switch priority {
+        case .high:
+            highPriorityTodos.append(item)
+        case .medium:
+            mediumPriorityTodos.append(item)
+        case .low:
+            lowPriorityTodos.append(item)
+        case .none:
+            nonePriorityTodos.append(item)
+        }
     }
     
     func newToDo() -> ChecklistItem {
         let item = ChecklistItem()
         item.text = randomTitle()
         item.checked = true
-        todos.append(item)
+        mediumPriorityTodos.append(item)
         return item
     }
     
     func move(item: ChecklistItem, to index: Int) {
-        guard let currentIndex = todos.firstIndex(of: item) else {
-            return
-        }
-        todos.remove(at: currentIndex)
-        todos.insert(item, at: index)
+//        guard let currentIndex = todos.firstIndex(of: item) else {
+//            return
+//        }
+//        todos.remove(at: currentIndex)
+//        todos.insert(item, at: index)
     }
     
-    func remove(items: [ChecklistItem]) {
-        for item in items {
-            if let index = todos.firstIndex(of: item) {
-                todos.remove(at: index)
-            }
+    
+    func remove(_ item: ChecklistItem, from priority: Priority, at index: Int) {
+        switch priority {
+        case .high:
+            highPriorityTodos.remove(at: index)
+        case .medium:
+            mediumPriorityTodos.remove(at: index)
+        case .low:
+            lowPriorityTodos.remove(at: index)
+        case .none:
+            nonePriorityTodos.remove(at: index)
         }
     }
+//    func remove(items: [ChecklistItem]) {
+//        for item in items {
+//            if let index = todos.firstIndex(of: item) {
+//                todos.remove(at: index)
+//            }
+//        }
+//    }
     
     private func randomTitle() -> String {
        let titles = ["New todo Item", "Generic todo", "Fill me out", "I need something to do", "Much todo about nothing"]
